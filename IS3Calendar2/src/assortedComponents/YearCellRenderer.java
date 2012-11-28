c06ce82f4cb990d5ea392065dedb25a26f47bdbc
Hector Grebbell <hectorgrebbell@googlemail.com>
2012-11-28 08:36:34 +0000

    IS3Calendar2 Added. See fb Post

---
+++ IS3Calendar2/src/assortedComponents/YearCellRenderer.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assortedComponents;
+
+import java.awt.Component;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+
+/**
+ *
+ * @author hector
+ */
+public class YearCellRenderer implements TableCellRenderer{
+  
+    public Component getTableCellRendererComponent(JTable table, Object value,
+        boolean isSelected, boolean hasFocus, int row, int column) {
+    YearCell feed = (YearCell)value;
+
+ 
+    YearCell panel = feed;
+
+    return panel;
+  }
+    
+}
