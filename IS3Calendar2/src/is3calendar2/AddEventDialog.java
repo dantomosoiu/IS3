9ad9ec74f7fe9e52bad63bd5fb951d7d93c18a47
Hector Grebbell <1007414g@bo720-9-05.(none)>
2012-11-29 12:32:11 +0000

    email crap added

--- IS3Calendar2/src/is3calendar2/AddEventDialog.java
+++ IS3Calendar2/src/is3calendar2/AddEventDialog.java
@@ -13,8 +13,18 @@ import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 import java.awt.Insets;
 import java.awt.Toolkit;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 /**
  *
@@ -204,8 +214,9 @@ public class AddEventDialog extends javax.swing.JDialog {
 
 
         cal.addAppointment(new Appointment(date, start, end, EventNameInput.getText(), LocationInput1.getText(), category.getSelectedIndex(), Appointment.RecurrenceFromInt(recurrence.getSelectedIndex()), 0));
-
+        sendEmail("noreply@is3calendar.com", InviteInput.getText(), "Test", "Test");
         cal.saveCalendar("./cal");
+        
 
         this.mainFrame.RefreshView();
         this.dispose();
@@ -261,6 +272,65 @@ public class AddEventDialog extends javax.swing.JDialog {
             }
         });
     }
+    
+    public void sendEmail(
+    String aFromEmailAddr, String aToEmailAddr,
+    String aSubject, String aBody
+  ){
+    //Here, no Authenticator argument is used (it is null).
+    //Authenticators are used to prompt the user for user
+    //name and password.
+    Session session = Session.getDefaultInstance( fMailServerConfig, null );
+    MimeMessage message = new MimeMessage( session );
+    try {
+      //the "from" address may be set in code, or set in the
+      //config file under "mail.from" ; here, the latter style is used
+      //message.setFrom( new InternetAddress(aFromEmailAddr) );
+      message.addRecipient(
+        Message.RecipientType.TO, new InternetAddress(aToEmailAddr)
+      );
+      message.setSubject( aSubject );
+      message.setText( aBody );
+      Transport.send( message );
+    }
+    catch (MessagingException ex){
+      System.err.println("Cannot send email. " + ex);
+    }
+  }
+     private static Properties fMailServerConfig = new Properties();
+
+  static {
+    fetchConfig();
+  }
+
+  /**
+  * Open a specific text file containing mail server
+  * parameters, and populate a corresponding Properties object.
+  */
+  private static void fetchConfig() {
+    InputStream input = null;
+    try {
+      //If possible, one should try to avoid hard-coding a path in this
+      //manner; in a web application, one should place such a file in
+      //WEB-INF, and access it using ServletContext.getResourceAsStream.
+      //Another alternative is Class.getResourceAsStream.
+      //This file contains the javax.mail config properties mentioned above.
+      input = new FileInputStream( "./mailconfig" );
+      fMailServerConfig.load( input );
+    }
+    catch ( IOException ex ){
+      System.err.println("Cannot open and load mail server properties file.");
+    }
+    finally {
+      try {
+        if ( input != null ) input.close();
+      }
+      catch ( IOException ex ){
+        System.err.println( "Cannot close mail server properties file." );
+      }
+    }
+  }
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JComboBox AMPMList;
     private javax.swing.JComboBox DayList;
